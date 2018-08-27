cbdaa660e0ac1a7a03a1606075ca1206f063662b
Emmanuel Xavier <emanuelsx@hotmail.com>
2018-08-27 18:30:21 -0300

    update

---
+++ Selectionsort-estatico/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#define TAM 5
+                  
+int vetor[TAM] = {5,4,3,2,1};
+
+void SelectionSort(int v[]){
+	int temp,i,j;
+	for (i = 0; i < TAM - 1; i++){
+		for (j = i + 1; j < TAM; j++){
+			if (v[j] < v[i]){
+				temp = v[j];
+				v[j] = v[i];
+				v[i] = temp;
+			}
+		}
+	}
+}
+
+void SelectionSortEstavel(int v[]){
+	int temp,i,j,menor;
+	for (i = 0; i < TAM - 1; i++){
+		menor = i;
+		for (j = i + 1; j < TAM; j++){
+			if (v[j] < v[menor]){
+				menor = j;
+			}
+		}
+		temp = v[menor];
+		v[menor] = v[i];
+		v[i] = temp;
+	}
+}
+
+
+void listar(int v[]){
+	int i;
+	
+	printf("\n\nListando\n\n");
+	for (i = 0; i < TAM; i++)
+		printf("%d ",v[i]);
+}
+
+
+int main(int argc, char **argv)
+{
+	listar(vetor);
+	SelectionSortEstavel(vetor);
+	listar(vetor);
+	
+	return 0;
+}
+
