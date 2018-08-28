40e633a65e04f6d84971a8caa2b99efa2b9e3606
Emmanuel Xavier <emanuelsx@hotmail.com>
2018-08-28 15:39:00 -0300

    atualiza selection dinâmico

--- selectionsort-dinamico /main.c
+++ selectionsort-dinamico /main.c
@@ -1,11 +1,13 @@
 #include "library.h"
 
+int trocas = 0;
 void selectionSort(TFila * primeiro){
 	TFila * atual, *proximo;
 	int temp;
 	for (atual = primeiro; atual->next != NULL; atual = atual->next){
 		for (proximo = atual->next; proximo != NULL; proximo = proximo->next){
 			if (proximo->info < atual->info){
+				trocas++;
 				temp = atual->info;
 				atual->info = proximo->info;
 				proximo->info = temp;
@@ -13,20 +15,39 @@ void selectionSort(TFila * primeiro){
 		}
 		
 	}
-	
+}
+
+void selectionSortEstavel(TFila * primeiro){
+	TFila * atual, *proximo, *menor;
+	int temp;
+	for (atual = primeiro; atual->next != NULL; atual = atual->next){
+		menor = atual;
+		for (proximo = atual->next; proximo != NULL; proximo = proximo->next){
+			if (proximo->info < menor->info){
+				menor = proximo;
+			}
+		}
+		if (atual != menor){
+			trocas++;
+			temp = atual->info;
+			atual->info = menor->info;
+			menor->info = temp;
+		}
+	}	
 }
 
 
 int main(int argc, char **argv)
 {
-	enfileirar(5);
-	enfileirar(4);
-	enfileirar(3);
-	enfileirar(2);
 	enfileirar(1);
+	enfileirar(2);
+	enfileirar(3);
+	enfileirar(4);
+	enfileirar(5);
 	listar();
-	selectionSort(fila);
+	selectionSortEstavel(fila);
 	listar();
+	printf("trocas %d",trocas);
 	return 0;
 }
 
